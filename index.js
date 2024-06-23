f98b1d42798261108a61c8800b31ceb939d2ba27
harshrai654 <harshraitth2@gmail.com>
2024-06-23 23:39:56 +0530

    pre-signed url generation and image upload

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
 const path = require("path");
+const {
+  BlobServiceClient,
+  BlobSASPermissions,
+} = require("@azure/storage-blob");
 const bodyParser = require("body-parser");
 const basicAuth = require("express-basic-auth");
 const express = require("express");
@@ -20,9 +24,36 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "static/index.html"));
 });
 
-app.post("/upload", (req, res) => {
-  console.log(req.body);
-  res.status(200);
+app.post("/upload", async (req, res) => {
+  try {
+    const { size, lastModified, name } = req.body;
+    const imageFolderPath = process.env.IMAGES_FOLDER_PATH;
+    const uniqueBlobName = `${imageFolderPath}${Date.now()}_${lastModified}_${size}_${name}`;
+    const blobServiceClient = BlobServiceClient.fromConnectionString(
+      process.env.AZURE_STORAGE_CONNECTION_STRING
+    );
+    const containerClient = blobServiceClient.getContainerClient(
+      process.env.AZURE_STORAGE_CONTAINER_NAME
+    );
+
+    const blobClient = containerClient.getBlobClient(uniqueBlobName);
+    const blobPermissions = new BlobSASPermissions();
+
+    blobPermissions.create = true;
+    blobPermissions.write = true;
+
+    const sasToken = await blobClient.generateSasUrl({
+      permissions: blobPermissions,
+      expiresOn: new Date(new Date().valueOf() + 5 * 60 * 1000),
+    });
+
+    res.json({
+      uploadUrl: sasToken,
+      blobName: uniqueBlobName,
+    });
+  } catch (error) {
+    console.error("[/UPLOAD]:", error);
+  }
 });
 
 app.listen(process.env.PORT || 3000, () => {
